c07c30c66d3a46298168ddf1b387d9a8481d402e
opadaaljondi <opada.m.aljondi@gmail.com>
2026-04-04 02:37:13 -0700

    dd

--- admin-dashboard/src/pages/teachers/TeachersList.jsx
+++ admin-dashboard/src/pages/teachers/TeachersList.jsx
@@ -88,7 +88,7 @@ const TeachersList = () => {
                 <Button
                     variant="primary"
                     icon={<Plus size={18} />}
-                    onClick={() => window.location.href = '/teachers/new'}
+                    onClick={() => navigate('/teachers/new')}
                 >
                     {t('teachers.addTeacher')}
                 </Button>
